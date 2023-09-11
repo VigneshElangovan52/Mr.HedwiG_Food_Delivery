c8c49209f1e9487165997aa18e3a8630645ad53d
Vignesh <vigneshe04@gmail.com>
2023-09-11 22:29:39 +0530

    Updated shimmer component

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,24 +1,98 @@
-import React from 'react';
+import React from "react";
 
 const Shimmer = () => {
   return (
-    <div className='flex flex-wrap my-10'>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
-        <div className='w-48 h-96 m-4 p-4 border border-solid rounded-md bg-slate-100'></div>
+    <div className="flex flex-wrap my-10">
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className="w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200">
+        <div className="w-40 h-28 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+      </div>
+      <div className='w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200'>
+          <div className='w-40 h-28 rounded-md border border-solid bg-stone-300'></div>
+          <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+          <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+          <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        </div>
+        <div className='w-48 h-1/3 m-4 p-4 border border-solid rounded-lg bg-slate-200'>
+          <div className='w-40 h-28 rounded-md border border-solid bg-stone-300'></div>
+          <div className="w-40 h-7 my-5 rounded-md border border-solid bg-stone-300"></div>
+          <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+          <div className="w-40 h-5 my-5 rounded-md border border-solid bg-stone-300"></div>
+        </div>
     </div>
   );
 };
