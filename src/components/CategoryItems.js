229a2f3729738f42e09db00d77d4a2fd7e26a691
Vignesh <vigneshe04@gmail.com>
2023-09-14 22:33:41 +0530

    Completed Item List and Item Category

--- src/components/CategoryItems.js
+++ src/components/CategoryItems.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { CDN_URL } from '../utils/constants';
 
-const CategoryItems = () => {
+const CategoryItems = ({data}) => {
+  console.log(data, "foodlist");
+
   return (
-    <div>CategoryItems</div>
-  )
+    <div>
+      {data.map((item) => (
+        <div className='p-5 m-2 border border-solid border-x-transparent border-t-transparent border-gray-500 shadow-lg flex justify-between rounded-lg'>
+          <div className='text-left' ><h1 className='font-semibold'>{item.card.info.name} - Rs.{item.card.info.price ? item.card.info.price/100 : item.card.info.defaultPrice/100 }</h1>
+          {item.card.info.description ? <p>{item.card.info.description}</p> : <p>Good Food,Good Life</p>}
+          </div>
+          <img className ="w-2/12 h-20" src = {CDN_URL + item.card.info.imageId} alt="food" />
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default CategoryItems;
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,3 @@
 export const MENU_URL = "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=11.034672&lng=77.039611&restaurantId=";
+
+export const CDN_URL = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
