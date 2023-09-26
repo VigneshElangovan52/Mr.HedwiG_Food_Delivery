df4d726d2a707f5f18679ce9d505fc13c57fba4a
Vignesh <vigneshe04@gmail.com>
2023-09-26 22:33:07 +0530

    Updated Contact us page

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,29 +1,23 @@
-const Contact = () => {
-  return (
+const Contact = () => (
+  <div className="m-3">
     <div>
-        <div>
-            <h1>Contact Us:</h1>
-            <p>We value your feedback and are here to assist you in any way possible. Feel free to reach out to us through the following channels:</p>
-            <h1>Customer Support:</h1>
-            <p>Our dedicated customer support team is available to address your inquiries, concerns, and feedback. You can contact us via:</p>
-            <ul>
-                <li>Phone: Call our helpline at 7894561238.</li>
-                <li>Email: Send us an email at support@mrfoodyapp.com.</li>
-                <li>Live Chat: Visit our website and engage in a live chat session with our support agents.</li>
-            </ul>
-            <h1>Office Address:</h1>
-<p>If you prefer a face-to-face interaction, you can visit our office located at:
-
-1,Downing St,Chennai-14<br /><br />
-
-Our team will be delighted to welcome you and address your needs in person. Please note that visits to our office are by appointment only.
-
-We are committed to enhancing your Mr.Foody experience and are eager to hear from you. Your input helps us grow and serve you better. 
-<br /><br />
-Thank you for choosing Mr.Foody as your culinary companion!</p>
-        </div>
+      <h1 className="font-bold py-2">Contact Us:</h1>
+      <p className="pb-5">We value your feedback and are here to assist you in any way possible. Feel free to reach out to us through the following channels:</p>
+      <h1 className="font-bold pb-3">Customer Support:</h1>
+      <p>Our dedicated customer support team is available to address your inquiries, concerns, and feedback. You can contact us via:</p>
+      <ul className="py-3">
+        <li><b>Phone📞:</b> Call our helpline at 7894561238.</li>
+        <li><b>Email📧:</b> Send us an email at support@mrfoodyapp.com.</li>
+        <li><b>Live Chat💬:</b> Visit our website and engage in a live chat session with our support agents.</li>
+      <li><b>Office Address🏢:</b>If you prefer a face-to-face interaction, you can visit our office located at: 1,Downing St,Chennai-14<br /><br /></li>
+      </ul>
+      <p>
+        Our team will be delighted to welcome you and address your needs in person. Please note that visits to our office are by appointment only
+        We are committed to enhancing your Mr.Foody experience and are eager to hear from you. Your input helps us grow and serve you better.</p>
+        <br /><br />
+        Thank you for choosing Mr.Foody as your culinary companion!
     </div>
-  )
-}
+  </div>
+)
 
 export default Contact;
